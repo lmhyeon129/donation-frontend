ad511ba34da9fd5cb6a6b936c89ef18dc278a6c0
Wonil Chung <wonill0718@gmail.com>
2019-07-24 20:08:09 +0900

    껍데기 완전 끝

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
-<!--    <div id="nav">
+    <!--    <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
--->
-    <router-view/>
+    -->
+    <div id="nav">
+      <router-link to="/">홈</router-link> |
+      <router-link to="/registerUser">회원가입(일반)</router-link> |
+      <router-link to="/registerCorp">회원가입(업체)</router-link> |
+      <router-link to="/productAdd">상품등록</router-link> |
+      <router-link to="/productList">상품목록</router-link> |
+      <router-link to="/requestDonation">기부요청</router-link> |
+      <router-link to="/productManage">기부관리</router-link> |
+    </div>
+    <router-view />
   </div>
 </template>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/views/ProductManage.vue
+++ src/views/ProductManage.vue
@@ -10,46 +10,54 @@
       </div>
     </div>
     <div class="row justify-content-center">
-      <div class="col col-auto">
-        <div class="row justify-content-center">
-          <div class="col">
-            <table class="table table-striped">
-              <thead class="thead-light">
-                <tr>
-                  <th scope="col">#</th>
-                  <th scope="col">상품명</th>
-                  <th scope="col">잔여수량</th>
-                  <th scope="col">상태</th>
-                  <th scope="col">체크</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>상품1</td>
-                  <td>1</td>
-                  <td>등록됨</td>
-                  <td>
-                    <div class="form-check">
-                      <input class="form-check-input" type="checkbox" name="target" id="gridCheck1" value="상품1" />
-                    </div>
-                  </td>
-                </tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td>상품2</td>
-                  <td>3</td>
-                  <td>기부예약</td>
-                  <td>
-                    <div class="form-check">
-                      <input class="form-check-input" type="checkbox" name="target" id="gridCheck2" value="상품2" />
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
+      <div class="col">
+        <table class="table table-striped">
+          <thead class="thead-light">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">상품명</th>
+              <th scope="col">잔여수량</th>
+              <th scope="col">상태</th>
+              <th scope="col">체크</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>상품1</td>
+              <td>1</td>
+              <td>등록됨</td>
+              <td>
+                <div class="form-check">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    name="target"
+                    id="gridCheck1"
+                    value="상품1"
+                  />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>상품2</td>
+              <td>3</td>
+              <td>기부예약</td>
+              <td>
+                <div class="form-check">
+                  <input
+                    class="form-check-input"
+                    type="checkbox"
+                    name="target"
+                    id="gridCheck2"
+                    value="상품2"
+                  />
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   </div>
